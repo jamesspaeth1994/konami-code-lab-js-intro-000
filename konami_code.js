b01bd3714a30c348ce3072ccc90bcc4341ec3d73
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-06-23 23:09:58 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,18 +2,22 @@ const codes = [38. 38. 40. 40. 37. 39. 37, 39, 66, 65];
 
 function init() {
   var index = 0;
-  document.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
+  document.addEventListener('keydown', onKeyDownHandler);
 
+  function onkeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    console.log('key', key);
     if (key === code[index]) {
       index++;
 
-      if (index === code.length - 1) {
-        window.alert("YOU DID IT!");
+      if (index === code.length) {
+        alert("YOU DID IT!");
         index = 0;
       }
     } else {
       index = 0;
     }
-  }, false);
+  }
 }
+
+init();
