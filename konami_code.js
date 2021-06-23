d63ed8553833fcbcb12d51787a3441b9d1f9a389
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-06-23 23:07:32 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,33 +1,19 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const codes = [38. 38. 40. 40. 37. 39. 37, 39, 66, 65];
 
 function init() {
-  // your code here
   var index = 0;
-  document.body.addEventListener("keydown", onKeyDownHandler);
-  function onKeyDownHandler(e) {
+  document.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
-    console.log("key", key);
+
     if (key === code[index]) {
       index++;
-      if (index === code.length) {
-        alert("YOU DID IT!");
+
+      if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
         index = 0;
       }
     } else {
       index = 0;
     }
-  }
+  }, false);
 }
-
-init();
